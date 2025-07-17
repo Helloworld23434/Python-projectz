f7c8990ae2dc2879de17ba5cd8fb47077013e299
Helloworld23434 <165587154+Helloworld23434@users.noreply.github.com>
2025-07-17 22:44:47 +0530

    Add files via upload

---
+++ AP generator.py
@@ -0,0 +1,5 @@
+x=int(input("Enter the first number of your AP series: "))
+a=int(input("Enter the last number of your AP series: "))
+b=int(input("Enter the common diffrence: "))
+for i in range(x,a+1,b):
+	print(i)
